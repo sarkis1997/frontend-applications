3ea813360bb634d7c92cb235977da0c396556d65
Sarkis Moeradjan <smoeradjan@gmail.com>
2019-10-18 19:05:20 +0200

    succeed to make a component

---
+++ components/fetch.svelte
@@ -0,0 +1,41 @@
+<script context="module">
+
+
+export function runTheQuery () {
+   let data = [];
+   const url = "https://api.data.netwerkdigitaalerfgoed.nl/datasets/ivo/NMVW/services/NMVW-07/sparql"
+   //Note that the query is wrapped in es6 template strings to allow for easy copy pasting
+   const query = `
+   PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>
+   PREFIX dc: <http://purl.org/dc/elements/1.1/>
+   PREFIX dct: <http://purl.org/dc/terms/>
+   PREFIX skos: <http://www.w3.org/2004/02/skos/core#>
+   PREFIX edm: <http://www.europeana.eu/schemas/edm/>
+   PREFIX foaf: <http://xmlns.com/foaf/0.1/>
+
+   SELECT ?cho ?title ?description ?objLabel ?img ?period WHERE {
+     ?cho edm:isRelatedTo <https://hdl.handle.net/20.500.11840/termmaster2647> .
+     ?cho dc:title ?title .
+       FILTER langMatches(lang(?title), "ned")
+     OPTIONAL { ?cho dc:description ?description } .
+     ?cho edm:object ?obj .
+       VALUES ?type { "gereedschap en uitrusting" "slavenketens" }
+     ?obj skos:prefLabel ?objLabel .
+     ?cho edm:isShownBy ?img .
+     ?cho dct:created ?period
+   }
+   `
+   runQuery(url, query)
+
+   function runQuery(url, query){
+     fetch(url+"?query="+ encodeURIComponent(query) +"&format=json")
+        .then(res => res.json())
+        .then(json => {
+            console.log(json.results.bindings);
+            data = json.results.bindings;
+     })
+   }
+}
+
+
+</script>
